06367ca2a9f1582d0331378fe215be4893aa503e
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2024-04-09 14:00:56 +0200

    generated backend

---
+++ src/main/java/com/example/webtechbackendbeispiel/WebtechBackendBeispielApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtechbackendbeispiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechBackendBeispielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechBackendBeispielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webtechbackendbeispiel/WebtechBackendBeispielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtechbackendbeispiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechBackendBeispielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
